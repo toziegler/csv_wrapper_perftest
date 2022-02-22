eaebb8c5c0008d2421bece2a32d83e4e2d1152a3
Ziegler, Tobias <tobias.ziegler@cs.tu-darmstadt.de>
2022-02-22 14:40:56 +0100

    fix lines in bw

--- wrapper_ib_send_bw.sh
+++ wrapper_ib_send_bw.sh
@@ -20,7 +20,7 @@ FILE="null" # will be constructed based on experiment name
 
 declare -A PERFTEST_PATH=( ["IB"]="./perftest/" ["EFA"]="/opt/perftest/bin/")
 declare -A ADDITIONAL_FLAGS=( ["IB"]="-R" ["EFA"]="-x 0")
-declare -A LINES=( ["IB"]="+18" ["EFA"]="+20")
+declare -A LINES=( ["IB"]="+18" ["EFA"]="+21")
 
 help(){
     echo "Usage:  TODO " >&2
