fdf897dd2c102f251372e75d1250a29b8a2379eb
Ziegler, Tobias <tobias.ziegler@cs.tu-darmstadt.de>
2022-02-23 17:55:59 +0100

    added paper plots

--- plots/plot_bw.r
+++ plots/plot_bw.r
@@ -7,6 +7,8 @@ library("wesanderson")
 library(ggthemes)
 library(readr)
 library(dplyr)
+library(gridExtra)
+library(knitr)
 options(scipen=999)
 
 df_ib = read.csv("../csv/IB_bw_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
@@ -18,8 +20,15 @@ df_efa$fabric = "EFA"
 #combine them into df
 df = rbind(df_efa,df_ib)
 
+df = sqldf("
+SELECT AVG (bwavg) as bwavg, AVG(msgrate) as msgrate, experiment, measurement, device, protocol, txdepth, rxdepth, cqmoderation, postlist, numberqps, inline, server, client, bytes, fabric
+FROM df
+GROUP BY experiment, measurement, device, protocol, txdepth, rxdepth, cqmoderation,
+postlist, numberqps, inline, server, client, bytes, fabric")
+
 df_bw_sync = sqldf("SELECT * FROM df WHERE experiment like '%bw_sync%'")
-ggplot(df_bw_sync,aes(x=bytes, y=bwavg, color=fabric)) +
+
+ggplot(sqldf("SELECT * from df_bw_sync WHERE bytes <= 8192"),aes(x=bytes, y=bwavg, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -35,12 +44,21 @@ ggplot(df_bw_sync,aes(x=bytes, y=bwavg, color=fabric)) +
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
 
+ggsave("sync_bw.pdf",width=8, height=4, device=cairo_pdf)
 
-df_bw_tx_depth = sqldf("SELECT * FROM df WHERE experiment like '%bw_tx_cq_grid%'")
 
+df_bw_tx_depth = sqldf(
+"SELECT *
+FROM df
+WHERE experiment like '%bw_tx_cq_grid%'
+AND (bytes=16
+or bytes=512
+or bytes=4096
+or bytes=8192)
+")
 
 # number of batches 
-ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1"),aes(x=txdepth, y=bwavg, color=fabric)) +
+p_bw = ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1 and bytes <=8192"),aes(x=txdepth, y=bwavg, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -56,9 +74,10 @@ ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1"),aes(x=txdepth,
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
+ggsave("tx_depth_bw.pdf",width=8, height=4, device=cairo_pdf)
 
 # number of batches 
-ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1"),aes(x=txdepth, y=msgrate, color=fabric)) +
+p_msgr = ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1 and bytes <=8192"),aes(x=txdepth, y=msgrate, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -69,17 +88,16 @@ ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE cqmoderation=1"),aes(x=txdepth,
     expand_limits(y=0) +
     xlab("tx depth") +
     ylab("msgrate [M]") +
-    theme(legend.position="top",
+    theme(legend.position="none",
           legend.title=element_blank(),
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
-
-
+ggsave("tx_depth_msgrate.pdf",width=8, height=4, device=cairo_pdf)
 
 
 # cq moderation msg rate 
-ggplot(df_bw_tx_depth,aes(x=cqmoderation, y=msgrate, color=fabric)) +
+ggplot(sqldf("SELECT * FROM df_bw_tx_depth WHERE txdepth=32 AND bytes=512" ),aes(x=cqmoderation, y=msgrate, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -95,7 +113,7 @@ ggplot(df_bw_tx_depth,aes(x=cqmoderation, y=msgrate, color=fabric)) +
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
-
+ggsave("cq_moderation_msgrate.pdf",width=8, height=4, device=cairo_pdf)
 
 # cq moderation bw
 ggplot(df_bw_tx_depth,aes(x=cqmoderation, y=bwavg, color=fabric)) +
@@ -126,7 +144,7 @@ ggplot(df_qps,aes(x=numberqps, y=bwavg, color=interaction(fabric,experiment))) +
     scale_colour_manual(values = wes_palette("Darjeeling1")) +
     scale_x_continuous(trans="log2") +
     expand_limits(y=0) +
-    xlab("cq moderation") +
+    xlab("number qps") +
     ylab("bandwidth [MB/s]") +
     theme(legend.position="top",
           legend.title=element_blank(),
@@ -135,8 +153,17 @@ ggplot(df_qps,aes(x=numberqps, y=bwavg, color=interaction(fabric,experiment))) +
           legend.box.margin=margin(-8,-10,-10,-10))
 
 
-df_qps = sqldf("SELECT * FROM df where experiment like '%bw_qps%'")
-ggplot(df_qps,aes(x=numberqps, y=msgrate, color=interaction(fabric,experiment))) +
+df_qps = sqldf(
+"SELECT *
+FROM df
+WHERE experiment like '%bw_qps'
+AND (bytes=16
+or bytes=512
+or bytes=4096
+or bytes=8192)
+")
+
+ggplot(df_qps,aes(x=numberqps, y=msgrate, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -152,11 +179,12 @@ ggplot(df_qps,aes(x=numberqps, y=msgrate, color=interaction(fabric,experiment)))
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
+ggsave("number_qps_msgrate.pdf",width=8, height=4, device=cairo_pdf)
 
+#as table 
 
-
-df_qps = sqldf("SELECT * FROM df where experiment like '%bw_post_list%'")
-ggplot(df_qps,aes(x=postlist, y=msgrate, color=interaction(fabric,experiment))) +
+df_post = sqldf("SELECT * FROM df where experiment like '%bw_post_list%'")
+ggplot(df_post,aes(x=postlist, y=msgrate, color=interaction(fabric,experiment))) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -172,3 +200,5 @@ ggplot(df_qps,aes(x=postlist, y=msgrate, color=interaction(fabric,experiment))) 
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
+
+kable(df_post, "latex", booktabs = TRUE)
--- plots/plot_latency.r
+++ plots/plot_latency.r
@@ -21,6 +21,41 @@ ggplot(df_srd_ud,aes(x=bytes, y=avg, color=protocol)) +
     scale_colour_manual(values = wes_palette("Darjeeling1")) +
     expand_limits(y=0) +
     scale_x_continuous(labels = scales::label_bytes(), trans="log2") +
+    xlab("message size") +
+    ylab(expression(paste("Latency [",mu,"s]"))) +
+    theme(legend.position="top",
+          legend.title=element_blank(),
+          text=element_text(size=22))
+          
+          ## legend.margin=margin(0,1,0,0),
+          ## legend.box.margin=margin(-8,-10,-10,-10))
+
+ggsave("latency_srd_ud.pdf",width=8, height=4, device=cairo_pdf)
+
+
+df_ib = read.csv("../csv/IB_lat_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
+df_ib$fabric="IB"
+
+df_efa = read.csv("../csv/EFA_lat_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
+df_efa$fabric="EFA"
+
+df_tcp = read.csv("../csv/TCP_lat_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
+df_tcp$fabric="TCP"
+
+df = rbind(df_efa, df_ib)
+
+df = rbind(df, df_tcp)
+# add tcp/ip numbers here
+
+# standard latency 
+ggplot(sqldf("SELECT * from df where experiment not like '%inline%' AND bytes >= 16 AND bytes <= 8192"),aes(x=bytes, y=avg, color=fabric)) +
+    ## geom_point(aes(shape=factor(q)),size=4) +
+    geom_point(size=4) +
+    geom_line(size=2, alpha=0.8) +
+    theme_bw() +
+    scale_colour_manual(values = wes_palette("Darjeeling1")) +
+    expand_limits(y=0) +
+    scale_x_continuous(labels = scales::label_bytes(), trans="log2") +
     xlab("message size [KB]") +
     ylab(expression(paste("Latency [",mu,"s]"))) +
     theme(legend.position="top",
@@ -28,23 +63,10 @@ ggplot(df_srd_ud,aes(x=bytes, y=avg, color=protocol)) +
           text=element_text(size=18),
           legend.margin=margin(0,1,0,0),
           legend.box.margin=margin(-8,-10,-10,-10))
+ggsave("latency_tcp_srd_ib.pdf",width=8, height=4, device=cairo_pdf)
 
-
-
-df_ib = read.csv("../csv/IB_lat_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
-colnames(df_ib) <- c(colnames(df_ib)[-1],"x")
-df_ib$x <- NULL
-df_ib$fabric="IB"
-
-df_efa = read.csv("../csv/EFA_lat_benchmark.csv", header=TRUE, sep=",", row.names=NULL)
-colnames(df_efa) <- c(colnames(df_efa)[-1],"x")
-df_efa$x <- NULL
-df_efa$fabric="EFA"
-
-
-df = rbind(df_efa, df_ib)
-
-ggplot(df,aes(x=bytes, y=avg, color=interaction(fabric,experiment))) +
+# 99 percentile 
+ggplot(sqldf("SELECT * from df where experiment not like '%inline%' AND bytes >= 16 AND bytes <= 8192"),aes(x=bytes, y=X99percentile, color=fabric)) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
@@ -61,11 +83,16 @@ ggplot(df,aes(x=bytes, y=avg, color=interaction(fabric,experiment))) +
           legend.box.margin=margin(-8,-10,-10,-10))
 
 
-ggplot(sqldf("SELECT * from df WHERE fabric like '%IB%'"),aes(x=bytes, y=avg, color=interaction(fabric,experiment))) +
+
+                                        #inline optimization - how to show normalize 
+
+ggplot(sqldf("SELECT * from df WHERE bytes < 512 AND (fabric like '%EFA%' OR fabric like '%IB%')")
+      ,aes(x=bytes, y=avg, color=interaction(experiment))) +
     ## geom_point(aes(shape=factor(q)),size=4) +
     geom_point(size=4) +
     geom_line(size=2, alpha=0.8) +
     theme_bw() +
+    facet_grid(fabric ~ .) + 
     scale_colour_manual(values = wes_palette("Darjeeling1")) +
     expand_limits(y=0) +
     scale_x_continuous(labels = scales::label_bytes(), trans="log2") +
